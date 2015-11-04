86c8035fdec23f9c549b6a2eb565938dde10a03f
Liam Cain <dev@liamca.in>
2015-11-04 12:49:03 -0500

    added generate_fen

--- ChessAI/Game.swift
+++ ChessAI/Game.swift
@@ -121,6 +121,52 @@ class Game {
         return true
     }
     
+    func generate_fen() -> String {
+        var empty = 0
+        var fen = ""
+        
+        for var i = SQUARES["a8"]!; i <= SQUARES["h1"]!; i++ {
+            if (board[i] == nil) {
+                empty++
+            } else {
+                if (empty > 0) {
+                    fen += empty
+                    empty = 0
+                }
+                var color = board[i].color
+                var piece = board[i].type
+                
+                fen += (color === WHITE) ?
+                    piece.toUpperCase() : piece.toLowerCase()
+            }
+            
+            if (i + 1) & 0x88 {
+                if empty > 0 {
+                    fen += empty
+                }
+                
+                if (i !== SQUARES["h1"]) {
+                    fen += "/"
+                }
+                
+                empty = 0
+                i += 8
+            }
+        }
+        
+        var cflags = ""
+        if (castling[WHITE]! & BITS.KSIDE_CASTLE.rawValue) { cflags += "K" }
+        if (castling[WHITE]! & BITS.QSIDE_CASTLE.rawValue) { cflags += "Q" }
+        if (castling[BLACK]! & BITS.KSIDE_CASTLE.rawValue) { cflags += "k" }
+        if (castling[BLACK]! & BITS.QSIDE_CASTLE.rawValue) { cflags += "q" }
+        
+        /* do we have an empty castling flag? */
+        cflags = cflags || "-"
+        var epflags = (ep_square == EMPTY) ? "-" : algebraic(ep_square)
+        
+        return [fen, turn, cflags, epflags, half_moves, move_number].componentsJoinedByString(" ")
+    }
+    
     
     func update_setup(fen: String) {
         if history.count > 0 {
