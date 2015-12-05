aee425a4ad44d8e39debc563d14876bba91b152b
zackmeath <zack.meath@gmail.com>
2015-12-05 11:51:59 -0500

    Adds material evaluation

--- ChessAI/Evaluate.swift
+++ ChessAI/Evaluate.swift
@@ -32,12 +32,60 @@ class Evaluate {
     
     var root: GameNode
     
+    // Claude Shannon(1949) values
+    let PIECE_VALUES: [GamePiece.Kind:Int] = [
+        GamePiece.Kind.PAWN   : 100,
+        GamePiece.Kind.KNIGHT : 300,
+        GamePiece.Kind.BISHOP : 300,
+        GamePiece.Kind.ROOK   : 500,
+        GamePiece.Kind.QUEEN  : 900,
+        GamePiece.Kind.KING: 999999,
+    ];
+    
+    // Larry Kaufman(2012) values
+    // let PIECE_VALUES: [GamePiece.Kind:Int] = [
+    //     GamePiece.Kind.PAWN   : 100,
+    //     GamePiece.Kind.KNIGHT : 350,
+    //     GamePiece.Kind.BISHOP : 350,
+    //     GamePiece.Kind.ROOK   : 525,
+    //     GamePiece.Kind.QUEEN  : 1000,
+    //     GamePiece.Kind.KING   : 999999,
+    // ];
+    
     init(game: Game) {
         root = GameNode(game: game)
     }
     
     func start() {
         step(root)
+    }
+    
+    func evaluateMaterial(node: GameNode) -> Int {
+        var whiteScore = 0
+        var blackScore = 0
+        let game = node.game
+        let board = game.board
+        let first_sq = board.SQUARES["a8"]
+        let last_sq = board.SQUARES["h1"]
+        
+        for var i = first_sq!; i <= last_sq!; i++ {
+            if let piece = board.get(i) {
+                if piece.side == GamePiece.Side.BLACK {
+                    blackScore += self.PIECE_VALUES[piece.kind]!
+                } else {
+                    whiteScore += self.PIECE_VALUES[piece.kind]!
+                }
+            }
+            if i % 8 == 7 {
+                i += 8
+            }
+        }
+        return whiteScore - blackScore;
+    }
+    
+    func evaluateNode(node: GameNode) -> Int{
+        let material = self.evaluateMaterial(node);
+        return material;
     }
     
     func step(node: GameNode){
